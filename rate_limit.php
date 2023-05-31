a51e22b5dec7dfc58e95431afddaa0c00026de0d
SegunCodes <shegstix64@gmail.com>
2023-05-31 16:22:33 +0200

    rate limit by endpoint and global rate limit

--- rate_limit.php
+++ rate_limit.php
@@ -3,10 +3,12 @@ require 'vendor/autoload.php';
 
 use Predis\Client;
 
+// Create a Redis connection
+$redis = new Client();
+
 // IP-based rate limiting using Redis
 function rateLimit($maxRequests, $timeWindow) {
-    // Create a Redis connection
-    $redis = new Client();
+    global $redis;
 
     // Get the client's IP address
     $clientIP = $_SERVER['REMOTE_ADDR'];
@@ -39,6 +41,82 @@ function rateLimit($maxRequests, $timeWindow) {
     echo "Request processed successfully.";
 }
 
+// Endpoint-based rate limiting
+function endpointRateLimit($maxRequests, $timeWindow)
+{
+    global $redis;
+
+    // Get the endpoint or route path
+    $endpoint = $_SERVER['REQUEST_URI'];
+
+    // Create a unique key for the endpoint
+    $key = 'rate_limit_' . $endpoint;
+
+    // Get the current request count for the endpoint
+    $requestCount = (int)$redis->get($key);
+
+    // If the request count doesn't exist or has expired, initialize it
+    if ($requestCount === 0) {
+        $redis->set($key, 1, 'ex', $timeWindow * 60); // Convert minutes to seconds
+        $redis->expire($key, $timeWindow);
+    } else {
+        // If the request count exists, increment it
+        $redis->incr($key);
+    }
+
+    // Check if the request count exceeds the maximum allowed requests
+    if ($requestCount >= $maxRequests) {
+        http_response_code(429); // 429 Too Many Requests
+        echo 'Endpoint limit exceeded. Please try again later.';
+        exit;
+    }
+}
+
+
+require 'vendor/autoload.php';
+
+use Predis\Client;
+
+// Global rate limiting using Redis
+function globalRateLimit($maxRequests, $timeWindow) {
+    global $redis;
+
+    // Set a unique key for the global rate limiting
+    $key = 'rate_limit_global';
+
+    // Get the current request count for the global rate limiting
+    $requestCount = $redis->get($key);
+
+    // If the request count doesn't exist or has expired, initialize it
+    if ($requestCount === null) {
+        $requestCount = 1;
+        $redis->set($key, $requestCount, 'ex', $timeWindow);
+    } else {
+        // If the request count exists, increment it
+        $requestCount++;
+        $redis->incr($key);
+    }
+
+    // Check if the request count exceeds the maximum allowed requests
+    if ($requestCount > $maxRequests) {
+        // Perform actions for rate limit exceeded, such as returning an error response or delaying the request
+        http_response_code(429); // 429 Too Many Requests
+        echo "Rate limit exceeded. Please try again later.";
+        exit;
+    }
+
+    // Proceed with normal execution if rate limit is not exceeded
+    echo "Request processed successfully.";
+}
+
+// Usage example: Global rate limit to 1000 requests per hour
+globalRateLimit(1000, 3600);
+
 // Usage example: Rate limit to 100 requests per minute
 rateLimit(100, 60);
+
+// Usage example with endpoint "/api/endpoint1"
+endpointRateLimit(10, 60);
+// Perform actions for the endpoint if rate limit is not exceeded
+echo 'Endpoint 1 response';
 ?>
--- readMe.md
+++ readMe.md
@@ -1,20 +1,34 @@
 ### RateLimit using PHP and Redis cache
 
+# IP-Based rate limit
 The rateLimit function establishes a connection with Redis using the Redis PHP extension.
 
-1. The client's IP address is retrieved using $_SERVER['REMOTE_ADDR'].
+1. The client's IP address is retrieved using `$_SERVER['REMOTE_ADDR']`.
 
 2. A unique key is generated based on the client's IP address to store the request count.
 
-3. The current request count for the IP address is fetched using $redis->get($key).
+3. The current request count for the IP address is fetched using `$redis->get($key)`
 
-4. If the request count doesn't exist or has expired, it is initialized to 1, stored in Redis using $redis->set(), and given an expiration time using $redis->expire().
+# Endpoint-based rate limit
+The endpointRateLimit function takes the maxRequests (maximum allowed requests within the timeWindow) and timeWindow (duration in seconds for rate limiting) as arguments.
 
-5. If the request count exists, it is incremented using $redis->incr().
+1. We retrieve the endpoint or route path from the `$_SERVER['REQUEST_URI']` superglobal variable.
 
-6. The code checks if the request count exceeds the maximum allowed requests. If so, it returns an error response (HTTP status code 429) and exits.
+2. We create a unique key for the endpoint using the $key variable.
 
-7. If the rate limit is not exceeded, the code proceeds with normal execution and outputs "Request processed successfully."
+3. We create a unique key for the endpoint using the $key variable.
+
+
+
+4. We connect to Redis using the Redis class and the appropriate connection details.
+
+5. If the request count doesn't exist or has expired, it is initialized to 1, stored in Redis using $redis->set(), and given an expiration time using $redis->expire().
+
+6. If the request count exists, it is incremented using $redis->incr().
+
+7. The code checks if the request count exceeds the maximum allowed requests. If so, it returns an error response (HTTP status code 429) and exits.
+
+8. If the rate limit is not exceeded, the code proceeds with normal execution and returns an output
 
 Remember to update the Redis server details ($redis->connect()) to match your Redis server configuration.
 
